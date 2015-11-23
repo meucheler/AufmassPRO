16b0e1d997c72e2ac2e4fe01e87200df64d03260
Michael Nimmervoll <Michael Nimmervoll>
2015-11-23 11:28:05 +0100

    EventHandling Anfang

--- AufmassPro/app/src/main/java/mikekevinsebitobi/aufmasspro/MainActivity.java
+++ AufmassPro/app/src/main/java/mikekevinsebitobi/aufmasspro/MainActivity.java
@@ -8,6 +8,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,15 +16,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        final Button button = (Button) findViewById(R.id.btnVerwaltung);
+        final Button button1= (Button) findViewById(R.id.btnAufmass);
+        final Button button2= (Button) findViewById(R.id.btnBuchhaltung);
+
+        //Verwaltung CLICKEVENT
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+            }
+        });
+
+        //Aufmass CLICKEVENT
+        button1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+            }
+        });
+
+        //Buchhaltung CLICKEVENT
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
             }
         });
     }
@@ -35,6 +53,15 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void btnVerClicked(View view) {
+
+    }
+    public void btnAufClick(View view) {
+
+    }
+    public void btnBucClick(View view) {
+
+    }
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
