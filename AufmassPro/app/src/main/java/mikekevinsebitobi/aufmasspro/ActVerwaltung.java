9300c96a2a5c9b559aeb12ac1eb0256f38ace7e3
MrBurnhead <Lasagne96>
2016-02-08 10:40:20 +0100

    GUI_ActListe

--- AufmassPro/app/src/main/java/mikekevinsebitobi/aufmasspro/ActVerwaltung.java
+++ AufmassPro/app/src/main/java/mikekevinsebitobi/aufmasspro/ActVerwaltung.java
@@ -1,10 +1,13 @@
 package mikekevinsebitobi.aufmasspro;
 
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class ActVerwaltung extends AppCompatActivity {
 
@@ -18,15 +21,38 @@ public class ActVerwaltung extends AppCompatActivity {
         ActionBar actBar = getSupportActionBar();
         actBar.setTitle("ActVerwaltung");
 
+        //Buttons
+        final Button btnKunden = (Button) findViewById(R.id.btnKunden);
+        final Button btnBauvorhaben = (Button) findViewById(R.id.btnProjekte);
+
+
+        //ClickListener
+        btnKunden.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+                startIntentListeKunden();
+
+            }
+        });
+
+        btnBauvorhaben.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform action on click
+
+            }
+        });
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_act_verwalt,menu);
+        getMenuInflater().inflate(R.menu.menu_act_verwalt, menu);
         return true;
     }
 
 
-    //ActionBar
-    //ActionBar bar = getSupportActionBar();
+    public void startIntentListeKunden(){
+        Intent intent = new Intent(this, ActListeKunden.class);
+        startActivity(intent);
+    }
 }
