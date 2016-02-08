e6f5297ba1f9a70e4c7d64c2f775adfb9355f07b
MrBurnhead <Lasagne96>
2016-02-08 08:10:25 +0100

    Erstellung UI in Pfad Verwaltung

---
+++ AufmassPro/app/src/main/java/mikekevinsebitobi/aufmasspro/ActListeKunden.java
@@ -0,0 +1,24 @@
+package mikekevinsebitobi.aufmasspro;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Seby on 01.02.16.
+ */
+public class ActListeKunden extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kunde);
+
+        //Action Bar
+
+        //ActionBar actBar = getActionBar();
+        //actBar.setTitle("ActVerwaltung");
+        //actBar.show();
+    }
+
+}
