27fe9dcec8bd8dcf059d6520102c4e5a3266e8c8
MrBurnhead <Lasagne96>
2016-02-22 10:44:13 +0100

    ListViewKunden anzeigen

--- AufmassPro/app/src/main/java/mikekevinsebitobi/aufmasspro/ActListeKunden.java
+++ AufmassPro/app/src/main/java/mikekevinsebitobi/aufmasspro/ActListeKunden.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.LinkedList;
 
@@ -18,7 +19,7 @@ import Logik.Logik;
  * Created by Seby on 01.02.16.
  */
 public class ActListeKunden extends AppCompatActivity{
-
+    LinkedList<String> spinnerItems;
     Logik logik;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,6 +35,15 @@ public class ActListeKunden extends AppCompatActivity{
         ActionBar actBar = getSupportActionBar();
         actBar.setTitle("Liste aller Kunden");
 
+        //Spinner
+        spinnerItems = new LinkedList<>();
+        spinnerItems.add("ID");
+        spinnerItems.add("Vorname");
+        spinnerItems.add("Nachname");
+
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_dropdown_item,spinnerItems);
+        Spinner sp =(Spinner) findViewById(R.id.spinnerKunden);
+        sp.setAdapter(spinnerArrayAdapter);
 
         // ListView
         LinkedList<Kunde> kundeLinkedList= logik.getKunden();
@@ -42,12 +52,33 @@ public class ActListeKunden extends AppCompatActivity{
         ArrayAdapter<Kunde> kundeArrayAdapter = new ArrayAdapter<Kunde>(this,android.R.layout.simple_list_item_1,kundeLinkedList);
 
         lv.setAdapter(kundeArrayAdapter);
+
+
+
     }
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_liste_kunden, menu);
         return true;
     }
 
+    public void filterListe(ListView lv, String filtTextVal, String filtSpinnVal){
 
+        LinkedList<Kunde> kundeLinkedList= logik.getKunden();
+        for (Kunde k:kundeLinkedList) {
+            if(filtSpinnVal.equals(spinnerItems.get(1)) && k.getAnsprechperson().getVorname().contains(filtTextVal)){
+                kundeLinkedList.remove(k);
+            }
+            if(filtSpinnVal.equals(spinnerItems.get(2)) && k.getAnsprechperson().getNachname().contains(filtTextVal)){
+                kundeLinkedList.remove(k);
+            }
+
+
+
+        }
+
+        ArrayAdapter<Kunde> kundeArrayAdapter = new ArrayAdapter<Kunde>(this,android.R.layout.simple_list_item_1,kundeLinkedList);
+
+        lv.setAdapter(kundeArrayAdapter);
+    }
 
 }
